4372f9e22013dea519cc4c4f8335678df830f9ad
imrtlfarm <imrtlfarm@gmail.com>
2022-06-22 12:00:32 -0700

    switch to stablecoin

--- src/test/FunkyLP.t.sol
+++ src/test/FunkyLP.t.sol
@@ -11,12 +11,12 @@ contract GreeterTest is Test {
     FunkyLP zap;
     uint amountA;
 
-    address tokenA = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
-    address tokenB = 0xF24Bcf4d1e507740041C9cFd2DddB29585aDCe1e;
+    address tokenA = 0x04068DA6C83AFCFA0e13ba15A6696662335D5B75;
+    address tokenB = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
 
-    address lp = 0x648a7452DA25B4fB4BDB79bADf374a8f8a5ea2b5;
+    address lp = 0x2b4C76d0dc16BE1C31D4C1DC53bF9B45987Fc75c;
 
-    address user =0x4901C740607E415685b4d09E4Aa960329cd183Ca;
+    address roosh = 0xdDf169Bf228e6D6e701180E2e6f290739663a784;
 
 
     event GMEverybodyGM();
@@ -31,21 +31,21 @@ contract GreeterTest is Test {
     // VM Cheatcodes can be found in ./lib/forge-std/src/Vm.sol
     // Or at https://github.com/foundry-rs/forge-std
     function testNormal() public {
-       vm.startPrank(user);
+       vm.startPrank(roosh);
        IERC20(tokenA).approve(address(zap), 1e33);
-       amountA = 1e22;
+       amountA = 1e6;
        zap.zap(tokenA, tokenB, amountA, false);
-       console.log(IERC20(lp).balanceOf(user), "LP TOKEN BALANCE");
+       console.log(IERC20(lp).balanceOf(roosh), "LP TOKEN BALANCE");
        vm.stopPrank();
     }
 
     function testFunky() public {
-       vm.startPrank(user);
+       vm.startPrank(roosh);
        IERC20(tokenA).approve(address(zap), 1e33);
-       amountA = 1e22;
+       amountA = 1e6    ;
        zap.zap(tokenA, tokenB, amountA, true);
 
-       console.log(IERC20(lp).balanceOf(user), "LP TOKEN BALANCE");
+       console.log(IERC20(lp).balanceOf(roosh), "LP TOKEN BALANCE");
        vm.stopPrank();
     }
 
